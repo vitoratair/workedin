32724e91038428c85ebef5e413be2300f58f86ad
Vitor Atair <vitor.ruts@gmail.com>
2014-10-26 08:29:04 -0200

    refactoring content

--- application/views/core/content.php
+++ application/views/core/content.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<h1 class="section-title">About me</h1>
 		<p class="section-description">
-		My name is Vitor, I'm software development since 2012, I am always looking for new challenges to put my knowledge and skills to proof.
+		My name is Vitor, I'm software development since 2012, I'm always looking for new challenges to put my knowledge and skills to proof.
 		</p>
 		<div class="row">
 			<div class="col-sm-4">
@@ -20,12 +20,79 @@
 			<div class="col-sm-4">
 				<h1>What Am I want</h1>
 				<p>
-				I'm specializing myself in mobile and web software development.
+				I'm specializing myself in mobile and web software development, always looking for new knowledges in this field to make better softwares.
 				</p>
 			</div>
 		</div>
 	</div>
 </section>
+
+		<section class="white-bg padding-top-bottom">
+
+			<div class="container features">
+
+				<h1 class="section-title">Main Skills</h1>
+				<p class="section-description">
+					These are some skills and technologies that I like to work with...
+				</p>
+
+				<div class="row">
+
+					<div class="col-sm-5 col-sm-offset-1 scrollimation fade-right">
+
+						<div class="media">
+							<div class="icon pull-right">
+								<i class="media-object icon-1 icon-python"></i>
+								<i class="media-object icon-2 fa fa-code"></i>
+							</div>
+
+							<div class="media-body text-right">
+								<h4><b>Python &amp; Django</b></h4>
+								<p>
+									These are my favorites, python it's a powerful and addictive language and django is a high-level web framework, it's simply amazing.
+								</p>
+							</div>
+						</div>
+
+						<div class="media">
+							<div class="icon pull-right">
+								<i class="media-object icon-1 fa fa-git"></i>
+								<i class="media-object icon-2 fa fa-github"></i>
+							</div>
+
+							<div class="media-body text-right">
+								<h4><b>GIT &amp; GitHub</b></h4>
+								<p>
+									With these technologies it's possible to do the code versioning and working at the same time together with any developer.
+								</p>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-sm-5 scrollimation fade-left">
+
+						<div class="media">
+							<div class="icon pull-left">
+								<i class="media-object icon-1 fa fa-html5"></i>
+								<i class="media-object icon-2 fa fa-css3"></i>
+							</div>
+
+							<div class="media-body">
+								<h4><b>HTML5 &amp; CSS3</b></h4>
+								<p>
+									With these technologies it's possible make beautiful and intuitive softwares, letting the user's and developer's life easier
+								</p>
+							</div>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+		</section>
 
 <footer id="main-footer">
 	<div class="container text-center">
