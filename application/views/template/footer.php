1df5016eb43f82203b4b0a203e74f2cee5fa1362
Vitor Atair <vitor.ruts@gmail.com>
2014-10-26 08:27:47 -0200

    deleted angular library

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -3,9 +3,6 @@
 		=============================================== -->
 		<script>window.jQuery || document.write('<script src="assets/js/libs/jquery-1.8.2.min.js">\x3C/script>')</script>
 
-		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.8/angular.min.js"></script>
-		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.0rc1/angular-route.min.js"></script>
-		<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.1.4/angular-resource.min.js"></script>
 		<script src="<?php echo base_url();?>assets/js/libs/bootstrap.min.js"></script>
 		<script src='<?php echo base_url();?>assets/js/jquery.easing.1.3.min.js'></script>
 		<script src='<?php echo base_url();?>assets/js/jquery.scrollto.js'></script>
