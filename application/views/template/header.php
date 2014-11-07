82b6928cc78b963799c9a6c591160061af011ce4
Vitor Atair <vitor.ruts@gmail.com>
2014-11-07 18:13:47 -0200

    added google maps

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -6,7 +6,7 @@
 		Title and Meta Tags
 		=============================================== -->
 		<meta charset="utf-8">
-		<title>Vitor Atair</title>
+		<title>Workedin</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
@@ -20,7 +20,7 @@
 		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/timeline.css">
 		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/styles-5.css">
 		<link rel="stylesheet" href="<?php echo base_url();?>assets/css/lockscreen.css">
-		<!-- <script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script> -->
+		<script src="http://maps.google.com/maps/api/js?sensor=false" type="text/javascript"></script>
 
 
 		<!-- ==============================================
