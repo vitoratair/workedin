736d1575ddc5d480f0a868b6db1d91cf8f3d767f
Vitor Atair <vitor.ruts@gmail.com>
2014-11-11 11:38:29 -0200

    added management page

---
+++ application/views/company/management.php
@@ -0,0 +1,116 @@
+<div id='cssmenu'>
+   <ul>
+      <li><a href='<?php echo base_url();?>index.php/company/home/'><span>Perfil</span></a></li>
+      <li><a href='<?php echo base_url();?>index.php/company/vacancy/'><span>Vagas</span></a></li>
+      <li class='active'><a href='<?php echo base_url();?>index.php/company/management/'><span>Gerenciamento</span></a></li>
+   </ul>
+</div>
+
+
+<section id="team" class="gray-bg padding-top-bottom">
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<h1 class="section-title">Gerencie seus candidatos</h1>
+				
+				<p align="center">
+		          <section>
+		            <div class="form-group" >
+		              <div class="input-group">
+		                <input list="salarios"
+		                	placeholder="Filtre somente os candidatos de vagas desejadas..." name="vagas" class="form-control"
+		                  style="box-shadow: 0 2px 1px #72c02c; border: 0px; height: 40px; background: #f3f3f3; border-radius: 0px">
+		                <datalist id="salarios">
+		                  <option value="Garçom">
+		                  <option value="Bla bla bla">
+		                  <option value="Oie oie oie">
+		                </datalist>
+		                <div class="input-group-addon" style="padding: 0px 0px ;border: 0px; background-color: transparent">
+		                  <a href="#" class="btn btn-u"style="padding: 12px 39px; font-size: 15px; box-shadow: 0 3px 1px #72c02c" >
+		                    <i class="fa fa-search"></i>
+		                  </a>
+		                </div>
+		              </div>
+		            </div>
+		          </section>
+				</p>
+				<br><br><br>
+
+				<div class="col-md-4 col-sm-6 team-member">
+					<div class="member-details">
+						<h2 align="center">Alân  Alân Alân</h2>
+						<p align="left">
+							<i class="fa fa-suitcase"></i>   Garçom<br>
+							<i class="fa fa-phone"></i>   (048) 84455 6485<br>
+							<i class="fa fa-envelope"></i>   teste@gmail.com<br>
+							<i class="fa fa-calendar"></i>  19/02/2014 - 18:00 - 
+							<a href="#">Alterar</a>
+						</p>
+						<br>
+						<p align="center">
+							<a class="btn btn-u" href="">Contratar</a>
+							<a class="btn btn-u" href="">Recusar</a>
+						</p>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 team-member">
+					<div class="member-details">
+						<h2 align="center">Steve Steve</h2>
+						<p align="left">
+							<i class="fa fa-suitcase"></i>   Garçom<br>
+							<i class="fa fa-phone"></i>   (048) 84455 6485<br>
+							<i class="fa fa-envelope"></i>   teste@gmail.com<br>
+							<i class="fa fa-calendar"></i>  19/02/2014 - 18:00 - 
+							<a href="#">Alterar</a>
+						</p>
+						<br>
+						<p align="center">
+							<a class="btn btn-u" href="">Contratar</a>
+							<a class="btn btn-u" href="">Recusar</a>
+						</p>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 team-member">
+					<div class="member-details">
+						<h2 align="center">Alân  Alân Alân</h2>
+						<p align="left">
+							<i class="fa fa-suitcase"></i>   Garçom<br>
+							<i class="fa fa-phone"></i>   (048) 84455 6485<br>
+							<i class="fa fa-envelope"></i>   teste@gmail.com<br>
+							<i class="fa fa-calendar"></i>  19/02/2014 - 18:00 - 
+							<a href="#">Alterar</a>
+						</p>
+						<br>
+						<p align="center">
+							<a class="btn btn-u" href="">Contratar</a>
+							<a class="btn btn-u" href="">Recusar</a>
+						</p>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6 team-member">
+					<div class="member-details">
+						<h2 align="center">Steve Steve</h2>
+						<p align="left">
+							<i class="fa fa-suitcase"></i>   Garçom<br>
+							<i class="fa fa-phone"></i>   (048) 84455 6485<br>
+							<i class="fa fa-envelope"></i>   teste@gmail.com<br>
+							<i class="fa fa-calendar"></i>  19/02/2014 - 18:00 - 
+							<a href="#">Alterar</a>
+						</p>
+						<br>
+						<p align="center">
+							<a class="btn btn-u" href="">Contratar</a>
+							<a class="btn btn-u" href="">Recusar</a>
+						</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+</div>
+</section>
