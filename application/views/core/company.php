b64d18e37bdb67222d579d724ef7fcca622d7d8b
Vitor Atair <vitor.ruts@gmail.com>
2014-12-02 16:31:55 -0200

    btn adicionar empresa

--- application/views/core/company.php
+++ application/views/core/company.php
@@ -117,8 +117,10 @@
                     </div>                    
 
                 </div>
-                <div class="col-md-3 col-md-offset-2">
-                    <img class="img-responsive" src="<?php echo base_url();?>assets/images/marker_green.png" alt="">
+                <div class="col-md-5" align="center">                         
+                    <img class="img-responsive" width="200px" align="center" src="<?php echo base_url();?>assets/images/marker_green.png" alt="">
+                    <br>
+                    <a class="btn-u btn-brd btn-u btn-u-lg" data-toggle="modal" data-target="#modal_quero_cadastrar"><i class="fa fa-unlink"></i> Quero cadastrar minha empresa</a>                    
                 </div>                
             </div>
         </div>        
@@ -158,53 +160,64 @@
 
 
 
-<div class="modal fade" id="modal_login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-    <div class="modal-dialog" style="margin-top: 100px;">
+<div class="modal fade" id="modal_quero_cadastrar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="margin-top: 150px">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                <h2 class="modal-title" id="myModalLabel">
-                    Login
-                </h2>
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h2 class="modal-title" id="myModalLabel">Cadastre-se sua empresa</h2>
             </div>
-            
-            <div class="modal-body">
-                <?php
-                $atributos = array('id'=>'form-login', 'class'=>'sky-form', 'method'=>'POST');
-                echo form_open('login/loginValidate', $atributos);
-                ?>
-                    <div class="row">
-                        <div class="col-md-10 col-md-offset-1">
-                            <br><br>
-                            <section class="col-md-12">
-                                <label class="input">
-                                    <input name="email" placeholder="Entre com seu E-mail" id="email" type="email" autocomplete="off">
-                                    <i class="icon-append fa fa-envelope"></i>
-                                </label>
-                            </section>
-                            
-                            <section class="col-md-12">
-                                <label class="input">
-                                    <i class="icon-append fa fa-lock"></i>
-                                    <input name="password" placeholder="Entre com sua senha" id="password" type="password" autocomplete="off">
-                                </label>
-                            </section>
-                            
-                            <p align="center">
-                                <label id="error"></label>
-                            </p>
-                            
-                          <div class="modal-footer">
-                              <button data-dismiss="modal" class="btn-u btn-u-default" type="button">Cancelar</button>
-                              <button class="btn-u" type="submit"><i class="fa fa-paper-plane"></i> Entrar</button>
-                          </div>                                                    
-                        </div>
+            <?php
+                $atributos = array('id'=>'form-add-email', 'class'=>'sky-form', 'method'=>'POST');
+                echo form_open('company/addCompany', $atributos);
+            ?>  
+                <br><br>
+
+                <div class="modal-body">
+                  <div class="row">
+                    <div class="col-md-10 col-md-offset-1">
+                      
+                      <section class="col-md-12">
+                        <label class="input">
+                          <i class="icon-append fa fa-envelope"></i>
+                          <input type="email" id="email" name="email" placeholder="Endereço de e-mail">
+                        </label>
+                      </section>
+                      
+                      <section class="col-md-12">
+                        <label class="input">
+                          <i class="icon-append fa fa-lock"></i>
+                          <input type="password" name="password" id="password" placeholder="Senha">
+                        </label>
+                      </section>
+
+                        <section class="col-md-12">
+                            <div class="inline-group">
+                              <label class="checkbox">
+                                  <input type="checkbox" name="contract" value="1" ><i></i>
+                                    <a href="<?php echo base_url();?>index.php/company/contract">
+                                      Li e aceito o contrato
+                                    </a>
+                              </label>
+                            </div>
+                        </section>                   
                     </div>
-                </form>
-            </div>
+                    <p align="center">
+                        <label id="error"></label>
+                    </p>                    
+                  </div>
+                      <div class="modal-footer">
+                          <button data-dismiss="modal" class="btn-u btn-u-default" type="button">Cancelar</button>
+                          <button class="btn-u" type="submit"><i class="fa fa-paper-plane"></i> Cadastrar</button>
+                      </div>
+                </div>
+
+                </div>
+            </form>
         </div>
     </div>
 </div>
+
 
 <?php 
     $msg = $this->session->userdata('msg');
@@ -215,19 +228,13 @@
 
     $( document ).ready(function() {
 
-        var msg = '<?php echo $msg;?>'      
-        console.log(msg);
-        if (msg == 'user_wrong')
-        {
-            $("#error").append('Nome de usuário ou senha estão inseridos de forma incorreta.');
-            $('#modal_login').modal('toggle');
-        }
-        else if (msg == 'email_equal')
+        var msg = '<?php echo $msg;?>'
+        
+        if (msg == 'email_equal')
         {
             $("#error").append('E-mail já cadastrado');
-            $('#modal_login').modal('toggle');          
+            $('#modal_quero_cadastrar').modal('toggle');
         }
-
     });
 
 </script>
