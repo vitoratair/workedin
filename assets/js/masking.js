e1fadec47baa70fac28fe018afa890e76e709c38
Vitor Atair <vitor.ruts@gmail.com>
2014-12-03 14:31:12 -0200

    ajustado cores e tamanho de fonte no menu principal e reirado placeholder das validações

--- assets/js/masking.js
+++ assets/js/masking.js
@@ -4,11 +4,11 @@ var Masking = function () {
         
         //Masking
         initMasking: function () {
-	        $("#phone").mask('(99) 9999-9999', {placeholder:'0'});
-	        $("#contactPhone").mask('(99) 9999-9999', {placeholder:'0'});
-	        $("#birth").mask('99/99/9999', {placeholder:'0'});
-	        $("#cpf").mask('999.999.999-99', {placeholder:'0'});
-	        $("#cnpj").mask('99.999.999/9999-99', {placeholder:'0'});
+	        $("#phone").mask('(99) 9999-9999');
+	        $("#contactPhone").mask('(99) 9999-9999');
+	        $("#birth").mask('99/99/9999');
+	        $("#cpf").mask('999.999.999-99');
+	        $("#cnpj").mask('99.999.999/9999-99');
 	        $("#salary").maskMoney({prefix:'R$ ' ,allowNegative: false, thousands:'.', precision:'0', decimal:',', affixesStay: false});
 
         }
